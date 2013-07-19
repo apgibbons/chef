84b93cb1ae59f7b1cccb65c20bdfa749bf1c164f
Andrew Gibbons <agibbons@gmail.com>
2013-07-19 02:27:04 -0700

    Create metadata.rb

---
+++ nagios-extras/metadata.rb
@@ -0,0 +1,10 @@
+maintainer        "Andrew Gibbons"
+maintainer_email  "agibbons@gmail.com"
+license           "Apache 2.0"
+description       "Installs and configures nagios extras"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.1"
+
+recipe "nagios::hoststate", "Updates notifications based on host environment"
+
+depends nagios
