927e3e8a26c248964457ed6959f8987d75b1d6f2
Andrew Gibbons <agibbons@gmail.com>
2013-07-19 02:28:21 -0700

    adds name

--- nagios-extras/metadata.rb
+++ nagios-extras/metadata.rb
@@ -4,6 +4,7 @@ license           "Apache 2.0"
 description       "Installs and configures nagios extras"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.1"
+name              "nagios-extras"
 
 recipe "nagios::hoststate", "Updates notifications based on host environment"
 
