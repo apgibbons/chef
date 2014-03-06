503b1f9e90abd606ec1d3e37346133fad531fda1
Andrew Gibbons <agibbons@gmail.com>
2014-03-06 13:55:53 -0800

    Delete process_file.sh

--- nagios-extras/templates/default/process_file.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-now=`date +%s`
-commandfile='<%= node['nagios']['state_dir'] %>/rw/nagios.cmd'
-
-/usr/bin/printf "[%lu] PROCESS_FILE;<%= node['nagios']['state_dir'] %>/rw/process_file;0\n" $now > $commandfile
